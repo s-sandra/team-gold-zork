dc8e18d4c34009ba92de99f921b6fc99c75f6d0d
Lewotten <Lewootten@gmail.com>
2017-04-05 11:42:38 -0400

    Changed class descriptions

--- src/team_gold_zork/ItemStealer.java
+++ src/team_gold_zork/ItemStealer.java
@@ -8,6 +8,10 @@ package team_gold_zork;
 import java.util.ArrayList;
 import java.io.PrintWriter;
 import java.util.Scanner;
+/**
+ * Stores information pertaining to ItemStealer type NPC
+ * @author Lauren
+ */
 public class ItemStealer extends Character{
     boolean hasItem = false; 
     Item lookingfor = null;
