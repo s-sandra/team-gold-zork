d64e0d83bb0e0a68176e99f70b73ee049e640b9e
Lewotten <Lewootten@gmail.com>
2017-04-05 12:13:36 -0400

    Added Character names

---
+++ src/team_gold_zork/CharacterSpecificCommand.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team_gold_zork;
+
+/**
+ *
+ * @author Lauren
+ * Deals with commands that can only be performed on certain objects.
+ **/
+ class CharacterSpecificCommand extends Command{
+    private String verb; //stores what a player wants to do with an object.
+    private String noun; //stores the name of the object a player wants to manipulate.
+    private Character character; //stores the character to perform an action on.
+
+
+    /**
+     * Constructs an ItemSpecificCommand.
+     * @param verb  the desired action to be performed on an Item.
+     * @param noun  the name of the Item to manipulate.
+     */
+    CharacterSpecificCommand(String verb, String noun){
+        this.verb = verb;
+        this.noun = noun;
+    }
+
+
+    /**
+//     * Executes a command to do something to an Character
+     * @return  the result of the command.
+     */
+    String execute(){
+    }
+    }
