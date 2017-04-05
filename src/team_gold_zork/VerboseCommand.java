76c578b65150220dc1ffa097ab59cd698772bd08
Lewotten <Lewootten@gmail.com>
2017-04-04 23:26:45 -0400

    Added VerboseCommand

---
+++ src/team_gold_zork/VerboseCommand.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team_gold_zork;
+
+/**
+ *
+ * @author Lauren
+ */
+public class VerboseCommand extends Command{
+   Room room = state.getAdventurersCurrentRoom();
+    /**
+     * creates VerboseComand 
+     */
+    VerboseCommand(){
+    }
+    
+    
+    /**
+     * Executes the command to set boolean isVerbose in Room to true 
+     * @return The result of the verboseCommand 
+     */
+    String execute(){
+        return "";
+    }
+}
