d84ec4fc5f6e9946760cb1a30ca9fb1f3db7fc00
s-sandra <aleksandra.shtabnaya@gmail.com>
2017-04-04 20:09:33 -0400

    Added the UnlockCommand.

---
+++ src/team_gold_zork/UnlockCommand.java
@@ -0,0 +1,24 @@
+package team_gold_zork;
+
+/**
+ * Deals with the user's attempt to open a locked door.
+ * @author Sandra Shtabnaya
+ */
+class UnlockCommand extends Command {
+    
+    /**
+     * Constructs an UnlockCommand object.
+     */
+    UnlockCommand(){
+        
+    }
+    
+    
+    /**
+     * Executes the command to unlock a closed door.
+     * @return The result of the unlock command. 
+     */
+    String execute(){
+        return "";
+    }
+}
