67e9b867ad89d8d1a10cc0f5ea65528370a65d6b
s-sandra <aleksandra.shtabnaya@gmail.com>
2017-04-06 19:18:51 -0400

    Added an internal Rank class in GameConfig to store the title and upper score limit for each rank in our rank system.

--- src/team_gold_zork/GameConfig.java
+++ src/team_gold_zork/GameConfig.java
@@ -13,4 +13,28 @@ class GameConfig {
     static final int MIN_THRESHOLD = 0;
     static final int MID_THRESHOLD = 0;
     static final int MAX_THRESHOLD = 0;
+
+    //List of ranks for the player.
+    static final Rank[] RANK = {new Rank(50, "amatuer")};
+}
+
+/**
+ * This class stores attributes related to a player's rank.
+ */
+class Rank {
+    private int upperRange; //the last point value of the rank.
+    private String title; //the name of the rank.
+
+    Rank(int upperRange, String title) {
+        this.upperRange = upperRange;
+        this.title = title;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    int getUpperRange() {
+        return upperRange;
+    }
 }
