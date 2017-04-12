c5b507b5041cf1c14a1214410fdf37ffcadaa115
Lewotten <Lewootten@gmail.com>
2017-04-12 12:15:23 -0400

    fixed

--- src/team_gold_zork/Player.java
+++ src/team_gold_zork/Player.java
@@ -50,6 +50,7 @@ class Player extends Character{
                                     w.print(",");
                             }
                     }
+                    w.write("\n");
             }
         w.write("Damage: " + getDamage() + "\n"); 
         w.write("Score: " + getScore() + "\n"); 
