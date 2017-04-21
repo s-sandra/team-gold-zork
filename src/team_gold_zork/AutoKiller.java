2bf9b2d7fbf1a02d3d1ed067e6d33c0e59f45767
Lewotten <Lewootten@gmail.com>
2017-04-20 21:36:51 -0400

    Started working on hydration

--- src/team_gold_zork/AutoKiller.java
+++ src/team_gold_zork/AutoKiller.java
@@ -34,7 +34,36 @@ class AutoKiller extends Character{
      * @throws IllegalSaveFormatException If the AutoKiller description contains invalid contents.
      */
     void restoreState(Scanner s)throws IllegalSaveFormatException{
-        
+         String line = s.nextLine();
+
+        //if the "Current room:" title is not found.
+        if(!line.startsWith("Current room:")){
+            throw new IllegalSaveFormatException();
+        }
+
+        //reads in the player's current room.
+        line = line.substring(line.indexOf(":") + 2); //chops off data to the left of colon.
+        currentRoom = currentDungeon.getRoom(line);
+        currentDungeon.setEntry(currentRoom);
+
+        line = s.nextLine();
+
+        //if the player had items in their inventory at save time (the "Inventory:" title would not appear otherwise).
+        if(line.startsWith("Inventory: ")){
+            line = line.substring(line.indexOf(":") + 2); //chops off data to the left of colon.
+
+            //chops off the commas from the inventory list.
+            String[] items = line.split(",");
+            for(String item: items){
+                try{ //the inventory might contain invalid items.
+                    this.inventory.add(currentDungeon.getItem(item));
+                }
+                catch(NoItemException e){
+                    throw new IllegalSaveFormatException(e.getMessage());
+                }
+            }
+            line = s.nextLine();
+    }
     }
    
     /**
